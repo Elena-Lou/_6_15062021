e3204fd9537901d8af9cf6d5f736491034d891b5
Elena_Lou <elouisiade@live.fr>
2021-06-15 17:16:54 +0200

    creation utilisateur et mdp

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const userRoutes = require("./Routes/user");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  next();
+});
+
+app.use((req, res, next) => {
+    console.log("requête reçue");
+    next();
+})
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+    res.json({ message: "requête reçue"});
+    next();
+})
+
+app.use((req, res, next) => {
+  console.log("Réponse envoyée avec succès !");
+});
+
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
